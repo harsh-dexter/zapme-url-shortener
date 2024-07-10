7b048f15c35018ea9ac2d052901aa67aa0cdce5c
harsh-dexter <harshtekriwal52@gmail.com>
2024-07-10 21:27:12 +0530

    added

--- server.js
+++ server.js
@@ -3,21 +3,36 @@ const express = require('express');
 const mongoose = require('mongoose');
 const ShortUrl = require('./models/shortUrl');
 const app = express();
+const path = require('path');
+
+
+const BASE_URL = process.env.VERCEL_URL ? `https://${process.env.VERCEL_URL}` : 'http://localhost:5000';
 
 mongoose.connect(process.env.MONGODB_URI, {
   useNewUrlParser: true,
-  useUnifiedTopology: true
+  useUnifiedTopology: true,
+  serverSelectionTimeoutMS: 5000,
+  socketTimeoutMS: 45000,
 }).then(() => console.log('Connected to MongoDB'))
   .catch(err => console.error('Could not connect to MongoDB', err));
 
+// ... rest of the server.js code remains the same
+
+
 app.set('view engine', 'ejs');
+// Set the views directory
+app.set('views', path.join(__dirname, 'views'));
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static('public'));
 
+app.use(express.static(path.join(__dirname, 'public')));
+
+
 app.get('/', async (req, res) => {
   const shortUrls = await ShortUrl.find().sort({ createdAt: 'desc' });
-  res.render('index', { shortUrls: shortUrls, baseUrl: process.env.BASE_URL });
+  res.render('index', { shortUrls: shortUrls, baseUrl: BASE_URL });
 });
+
 
 app.post('/shortUrls', async (req, res) => {
   await ShortUrl.create({ full: req.body.fullUrl });
